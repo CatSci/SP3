735afd3a1ed1a6672e6733c150a8bc038c3d712a
CatSci <atul.yadav@catsci.com>
2023-05-26 11:25:55 +0100

    theme changed

--- run.py
+++ run.py
@@ -9,6 +9,59 @@ import plotly.graph_objects as go
 
 from utils import chromotogram_data, bubble_plot, bubble_figure_data
 
+
+
+# hide streamlit style
+hide_st_style = """
+            <style>
+            #MainMenu {visibility: hidden;}
+            footer {visibility: hidden;}
+            header {visibility: hidden;}
+            </style>
+            """
+st.markdown(hide_st_style, unsafe_allow_html=True)
+
+
+st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
+# background-color: #ed9439;
+st.markdown("""
+<style>
+.navbar {
+  height: 80px;
+  background-color: #ed9439;
+  color: #ed9439;
+}
+.navbar-brand{
+    font-size: 40px;
+    margin-left:40px;
+}
+</style>""", unsafe_allow_html= True)
+
+
+st.markdown("""
+<nav class="navbar fixed-top navbar-expand-lg navbar-dark">
+  <a class="navbar-brand" href="#" target="_blank">CatSci</a>
+  
+
+</nav>
+""", unsafe_allow_html=True)
+
+
+
+st.markdown("""
+<style>
+div.stButton > button:first-child {
+    background-color: #ed9439;
+    color:#0f1b2a;
+    border:None;
+}
+div.stButton > button:first-child:focus {
+    background-color: #ed9439;
+    color:#0f1b2a;
+    border:None;
+}
+</style>""", unsafe_allow_html=True)
+
 st.markdown('**Note - Please do not post target or intermediate structure information externally**.')
 st.title('SP3 Table')
 
@@ -189,56 +242,6 @@ def create_sp3_table():
      return sp3_table_df.to_csv().encode('utf-8'), chromotogram_df, bubble_df, final_df.to_csv().encode('utf-8')
 
 
-# hide streamlit style
-hide_st_style = """
-            <style>
-            #MainMenu {visibility: hidden;}
-            footer {visibility: hidden;}
-            header {visibility: hidden;}
-            </style>
-            """
-st.markdown(hide_st_style, unsafe_allow_html=True)
-
-
-st.markdown('<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">', unsafe_allow_html=True)
-# background-color: #ed9439;
-st.markdown("""
-<style>
-.navbar {
-  height: 80px;
-  background-color: #ed9439;
-  color: #ed9439;
-}
-.navbar-brand{
-    font-size: 40px;
-    margin-left:40px;
-}
-</style>""", unsafe_allow_html= True)
-
-
-st.markdown("""
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark">
-  <a class="navbar-brand" href="#" target="_blank">CatSci</a>
-  
-
-</nav>
-""", unsafe_allow_html=True)
-
-
-
-st.markdown("""
-<style>
-div.stButton > button:first-child {
-    background-color: #ed9439;
-    color:#0f1b2a;
-    border:None;
-}
-div.stButton > button:first-child:focus {
-    background-color: #ed9439;
-    color:#0f1b2a;
-    border:None;
-}
-</style>""", unsafe_allow_html=True)
 
 if st.button('Create SP3 Table'):
     sp3, chromotogram_df, bubble_df, final_df = create_sp3_table()
